29553a575092feba2f6d6226f3ee6020223bc072
Tiago Lima <128660268+TiagoDSLima@users.noreply.github.com>
2025-03-10 22:31:23 -0300

    Criando projeto

---
+++ src/main/java/br/com/tiagolima/curso_api_rest_java_spring_boot/CursoApiRestJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagolima.curso_api_rest_java_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApiRestJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoApiRestJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tiagolima/curso_api_rest_java_spring_boot/CursoApiRestJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tiagolima.curso_api_rest_java_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApiRestJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
