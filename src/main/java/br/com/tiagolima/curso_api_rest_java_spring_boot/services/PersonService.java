74ed5113ff1e8984fdfc004a8fdcb07230e60e21
TiagoDSLima <tiagodslima51@gmail.com>
2025-03-26 22:28:12 -0300

    criando método findAll

--- src/main/java/br/com/tiagolima/curso_api_rest_java_spring_boot/services/PersonService.java
+++ src/main/java/br/com/tiagolima/curso_api_rest_java_spring_boot/services/PersonService.java
@@ -3,6 +3,8 @@ package br.com.tiagolima.curso_api_rest_java_spring_boot.services;
 import br.com.tiagolima.curso_api_rest_java_spring_boot.models.PersonModel;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,15 @@ public class PersonService {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonService.class.getName());
+
+    public List<PersonModel> findAll(){
+        List<PersonModel> people = new ArrayList<>();
+        for(int i = 0; i<8; i++){
+            PersonModel person = mockPerson(i);
+            people.add(person);
+        }
+        return people;
+    }
 
     public PersonModel findById(String id){
         logger.info("Finding one person");
@@ -23,4 +34,15 @@ public class PersonService {
         person.setGender("Male");
         return person;
     }
+
+    private PersonModel mockPerson(int i) {
+        PersonModel person = new PersonModel();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person" + i);
+        person.setLastName("LastName" + i);
+        person.setAddress("Address " + i);
+        person.setGender("Gender" + i);
+        return person;
+    }
+
 }
